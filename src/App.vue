1c5340fb5451e9d6be855ce62a83efd5a56cd316
iamdeveloper <danlex@localhost.localdomain>
2018-12-31 11:53:29 +0100

    finished the app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main class="mainWrapper">
+      <Menu @toggle-menu="toggleMenu" :menuOn="menuOn"/>
+      <Heading />
+      <Claim />
+      <Form />
+      <Img />
+    </main>
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Claim from './components/Claim.vue';
+import Footer from './components/Footer.vue';
+import Form from './components/Form.vue';
+import Heading from './components/Heading.vue';
+import Img from './components/Image.vue';
+import Menu from './components/Menu.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    Claim,
+    Footer,
+    Form,
+    Heading,
+    Img,
+    Menu,
   },
+
+  data() {
+    return {
+      menuOn: false,
+    }
+  },
+
+  methods: {
+    toggleMenu() {
+      this.menuOn = !this.menuOn;
+    }
+  }
 };
 </script>
 
 <style lang="scss">
+
+html {
+  box-sizing: border-box;
+  height: 100%;
+}
+*, *:before, *:after {
+  box-sizing: inherit;
+  padding: 0;
+  margin: 0;
+  -webkit-tap-highlight-color: transparent;
+}
+
 #app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+   @media(min-width: 1024px) {
+    width: 1024px;
+    margin: auto;
+  }
+}
+
+.mainWrapper {
+  flex: 1 0 auto;
+
+  display: flex;
+  flex-direction: column;
+
+  & > * {
+    order: 999;
+  }
 }
 </style>
---
+++ src/components/Image.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="imageWrapper">
+        <img src="http://lorempixel.com/568/320/sports" alt="">
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .imageWrapper {
+
+        padding: 12px;
+        margin-top: 20px;
+        background-image: linear-gradient(to bottom right, #9f64e7, #6960f4);
+
+        @media(min-width: 768px) {
+            padding: 35px;
+        }
+
+        @media(min-width: 1024px) {
+            order: 3;
+        }
+
+            img {
+                width: 100%;
+            }
+    }
+</style>
